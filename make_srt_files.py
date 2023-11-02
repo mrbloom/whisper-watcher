db354b05474d09ae34e610129a71e6c00d4ebf05
mrbloom <mrbloom@ukr.net>
2023-11-02 10:43:10 +0200

    add logging works only make_srt_files.py

--- make_srt_files.py
+++ make_srt_files.py
@@ -4,31 +4,27 @@ import subprocess
 from glob import glob
 import sys
 import time
+import logging  # Import the logging module
 
+# 1. Logging configuration
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    handlers=[logging.FileHandler("subtitles_log.log"),
+                              logging.StreamHandler()])
 
 def get_input(prompt, default_value):
     """Get input from the user with a default value."""
     value = input(f"{prompt} (default: {default_value}): ")
     return value or default_value
 
-
-# Check for directory argument. If not provided, use current directory.
 dir_path = sys.argv[1] if len(sys.argv) > 1 else get_input("Set directory for files", ".")
-
-# Check for mask argument. If not provided, use *.mp4.
 mask = sys.argv[2] if len(sys.argv) > 2 else get_input("Set mask for files", "**/*.ts")
 
-
 def get_file_size(filepath):
-    # open the file in read only
     with open(filepath, "r") as file:
-        # move pointer to the end of the file
         file.seek(0, 2)
-        # retrieve the current position of the pointer
-        # this will be the file's size in bytes
         size = file.tell()
         return size
-
 
 def is_file_ready(filepath):
     try:
@@ -41,44 +37,39 @@ def is_file_ready(filepath):
     except OSError:
         return False
 
-
 while True:
-    # Iterate over all files based on mask in the specified directory and subdirectories
     search_dir = os.path.join(dir_path, mask)
-    for file in glob(search_dir, recursive=True):  # Added recursive=True to search in subdirectories
+    for file in glob(search_dir, recursive=True):
         if is_file_ready(file):
             srt_file = os.path.join(os.path.splitext(file)[0] + '.srt')
-
             if not os.path.exists(srt_file) and not os.path.exists(f"{srt_file}.dummy"):
-                print(f"Processing: {file}")
+                logging.info(f"Processing: {file}")  # Log instead of print
 
                 with open(f"{srt_file}.dummy", 'w') as dummy:
                     dummy.write("PROCESSING...")
 
                 try:
-                    # Start whisper in the same window
                     cmd = f'whisper --model large-v2 "{file}" --output_dir "{dir_path}"'
-                    start_time = time.time()  # Capture the start time
+                    start_time = time.time()
                     subprocess.run(cmd, shell=True)
-                    end_time = time.time()  # Capture the end time
+                    end_time = time.time()
 
                     duration = end_time - start_time
                     duration_td = datetime.timedelta(seconds=duration)
-                    dt = datetime.datetime(1, 1, 1) + duration_td  # Adding to a minimum datetime value
+                    dt = datetime.datetime(1, 1, 1) + duration_td
 
                     formatted_time = dt.strftime("%H:%M:%S")
-                    print(f"Subtitles for {file} created in {formatted_time}.")
+                    logging.info(f"Subtitles for {file} created in {formatted_time}.")  # Log instead of print
 
-                    # Sleep for a short duration before processing the next file
                     while not os.path.exists(srt_file):
                         time.sleep(2)
 
                     os.remove(f"{srt_file}.dummy")
                     os.remove(file)
                 except:
-                    print(f"Some problems with {file}")
+                    logging.error(f"Some problems with {file}")  # Log error instead of print
             else:
-                print(f"SRT file already exists for: {file} or dummy file. Check video file.")
+                logging.warning(f"SRT file already exists for: {file} or dummy file. Check video file.")  # Log warning instead of print
 
-    print("Done!")
+    logging.info("Done!")  # Log instead of print
     time.sleep(10)
